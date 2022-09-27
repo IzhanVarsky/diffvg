17c0f87964fa12dc73b7ee81c4440f620d70e02b
IzhanVarsky <48652648+IzhanVarsky@users.noreply.github.com>
2022-09-27 17:05:11 +0300

    repaired save svg for svg with multipaths

--- pydiffvg/save_svg.py
+++ pydiffvg/save_svg.py
@@ -76,80 +76,89 @@ def svg_to_tree(width, height, shapes, shape_groups, use_gamma=False):
             add_color(shape_group.stroke_color, 'shape_{}_stroke'.format(i))
 
     for i, shape_group in enumerate(shape_groups):
-        # WHY only `shape_ids[0]`? What about others??
-        shape = shapes[shape_group.shape_ids[0]]
-        if isinstance(shape, pydiffvg.Circle):
-            shape_node = etree.SubElement(g, 'circle')
-            shape_node.set('r', str(shape.radius.item()))
-            shape_node.set('cx', str(shape.center[0].item()))
-            shape_node.set('cy', str(shape.center[1].item()))
-        elif isinstance(shape, pydiffvg.Polygon):
-            shape_node = etree.SubElement(g, 'polygon')
-            points = shape.points.data.cpu().numpy()
-            path_str = ''
-            for j in range(0, shape.points.shape[0]):
-                path_str += '{} {}'.format(points[j, 0], points[j, 1])
-                if j != shape.points.shape[0] - 1:
-                    path_str += ' '
-            shape_node.set('points', path_str)
-        elif isinstance(shape, pydiffvg.Path):
-            shape_node = etree.SubElement(g, 'path')
-            num_segments = shape.num_control_points.shape[0]
-            num_control_points = shape.num_control_points.data.cpu().numpy()
-            points = shape.points.data.cpu().numpy()
-            num_points = shape.points.shape[0]
-            path_str = 'M {} {}'.format(points[0, 0], points[0, 1])
-            point_id = 1
-            for j in range(0, num_segments):
-                if num_control_points[j] == 0:
-                    p = point_id % num_points
-                    path_str += ' L {} {}'.format( \
-                        points[p, 0], points[p, 1])
-                    point_id += 1
-                elif num_control_points[j] == 1:
-                    p1 = (point_id + 1) % num_points
-                    path_str += ' Q {} {} {} {}'.format( \
-                        points[point_id, 0], points[point_id, 1],
-                        points[p1, 0], points[p1, 1])
-                    point_id += 2
-                elif num_control_points[j] == 2:
-                    p2 = (point_id + 2) % num_points
-                    path_str += ' C {} {} {} {} {} {}'.format( \
-                        points[point_id, 0], points[point_id, 1],
-                        points[point_id + 1, 0], points[point_id + 1, 1],
-                        points[p2, 0], points[p2, 1])
-                    point_id += 3
-            shape_node.set('d', path_str)
-        elif isinstance(shape, pydiffvg.Rect):
-            shape_node = etree.SubElement(g, 'rect')
-            shape_node.set('x', str(shape.p_min[0].item()))
-            shape_node.set('y', str(shape.p_min[1].item()))
-            shape_node.set('width', str(shape.p_max[0].item() - shape.p_min[0].item()))
-            shape_node.set('height', str(shape.p_max[1].item() - shape.p_min[1].item()))
-        else:
-            assert (False)
+        is_all_paths = True
+        for shape_num in range(len(shape_group.shape_ids)):
+            if not isinstance(shapes[shape_group.shape_ids[shape_num]], pydiffvg.Path):
+                is_all_paths = False
+                break
+        if is_all_paths:
+            shape_path_node = etree.SubElement(g, 'path')
+        # print("LEN OF SHAPE GROUP:", len(shape_group.shape_ids))
+        for shape_num in range(len(shape_group.shape_ids)):
+            shape = shapes[shape_group.shape_ids[shape_num]]
+            if isinstance(shape, pydiffvg.Circle):
+                shape_node = etree.SubElement(g, 'circle')
+                shape_node.set('r', str(shape.radius.item()))
+                shape_node.set('cx', str(shape.center[0].item()))
+                shape_node.set('cy', str(shape.center[1].item()))
+            elif isinstance(shape, pydiffvg.Polygon):
+                shape_node = etree.SubElement(g, 'polygon')
+                points = shape.points.data.cpu().numpy()
+                path_str = ''
+                for j in range(0, shape.points.shape[0]):
+                    path_str += '{} {}'.format(points[j, 0], points[j, 1])
+                    if j != shape.points.shape[0] - 1:
+                        path_str += ' '
+                shape_node.set('points', path_str)
+            elif isinstance(shape, pydiffvg.Path):
+                shape_node = shape_path_node if is_all_paths else etree.SubElement(g, 'path')
+                num_segments = shape.num_control_points.shape[0]
+                num_control_points = shape.num_control_points.data.cpu().numpy()
+                points = shape.points.data.cpu().numpy()
+                num_points = shape.points.shape[0]
+                path_str = 'M {} {}'.format(points[0, 0], points[0, 1])
+                point_id = 1
+                for j in range(0, num_segments):
+                    if num_control_points[j] == 0:
+                        p = point_id % num_points
+                        path_str += ' L {} {}'.format(points[p, 0], points[p, 1])
+                        point_id += 1
+                    elif num_control_points[j] == 1:
+                        p1 = (point_id + 1) % num_points
+                        path_str += ' Q {} {} {} {}'.format(
+                            points[point_id, 0], points[point_id, 1],
+                            points[p1, 0], points[p1, 1])
+                        point_id += 2
+                    elif num_control_points[j] == 2:
+                        p2 = (point_id + 2) % num_points
+                        path_str += ' C {} {} {} {} {} {}'.format(
+                            points[point_id, 0], points[point_id, 1],
+                            points[point_id + 1, 0], points[point_id + 1, 1],
+                            points[p2, 0], points[p2, 1])
+                        point_id += 3
+                old_D_value = shape_node.get('d', default="")
+                prefix_D = (old_D_value + " ") if old_D_value != "" else ""
+                shape_node.set('d', prefix_D + path_str)
+            elif isinstance(shape, pydiffvg.Rect):
+                shape_node = etree.SubElement(g, 'rect')
+                shape_node.set('x', str(shape.p_min[0].item()))
+                shape_node.set('y', str(shape.p_min[1].item()))
+                shape_node.set('width', str(shape.p_max[0].item() - shape.p_min[0].item()))
+                shape_node.set('height', str(shape.p_max[1].item() - shape.p_min[1].item()))
+            else:
+                assert False
 
-        shape_node.set('stroke-width', str(2 * shape.stroke_width.data.cpu().item()))
-        if shape_group.fill_color is not None:
-            if isinstance(shape_group.fill_color, pydiffvg.LinearGradient):
-                shape_node.set('fill', 'url(#shape_{}_fill)'.format(i))
+            shape_node.set('stroke-width', str(2 * shape.stroke_width.data.cpu().item()))
+            if shape_group.fill_color is not None:
+                if isinstance(shape_group.fill_color, pydiffvg.LinearGradient):
+                    shape_node.set('fill', 'url(#shape_{}_fill)'.format(i))
+                else:
+                    c = shape_group.fill_color.data.cpu().numpy()
+                    shape_node.set('fill', 'rgb({}, {}, {})'.format( \
+                        int(255 * c[0]), int(255 * c[1]), int(255 * c[2])))
+                    shape_node.set('opacity', str(c[3]))
             else:
-                c = shape_group.fill_color.data.cpu().numpy()
-                shape_node.set('fill', 'rgb({}, {}, {})'.format( \
-                    int(255 * c[0]), int(255 * c[1]), int(255 * c[2])))
-                shape_node.set('opacity', str(c[3]))
-        else:
-            shape_node.set('fill', 'none')
-        if shape_group.stroke_color is not None:
-            if isinstance(shape_group.stroke_color, pydiffvg.LinearGradient):
-                shape_node.set('stroke', 'url(#shape_{}_stroke)'.format(i))
-            else:
-                c = shape_group.stroke_color.data.cpu().numpy()
-                shape_node.set('stroke', 'rgb({}, {}, {})'.format( \
-                    int(255 * c[0]), int(255 * c[1]), int(255 * c[2])))
-                shape_node.set('stroke-opacity', str(c[3]))
-            shape_node.set('stroke-linecap', 'round')
-            shape_node.set('stroke-linejoin', 'round')
+                shape_node.set('fill', 'none')
+            if shape_group.stroke_color is not None:
+                if isinstance(shape_group.stroke_color, pydiffvg.LinearGradient):
+                    shape_node.set('stroke', 'url(#shape_{}_stroke)'.format(i))
+                else:
+                    c = shape_group.stroke_color.data.cpu().numpy()
+                    shape_node.set('stroke', 'rgb({}, {}, {})'.format( \
+                        int(255 * c[0]), int(255 * c[1]), int(255 * c[2])))
+                    shape_node.set('stroke-opacity', str(c[3]))
+                shape_node.set('stroke-linecap', 'round')
+                shape_node.set('stroke-linejoin', 'round')
     return root
 
 
